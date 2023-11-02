985f4035edaaa625f720597f0ed0f389cb517f8d
Mahdy Darroudy <146613339+darroudi82@users.noreply.github.com>
2023-11-02 23:37:51 +0330

    Update header.php

---
+++ php-exercise12/header.php
@@ -0,0 +1,9 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>welcome to class second of php</h1>
+    <p>in this class we will learn php languege</p>
