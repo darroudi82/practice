359f4e951854ea99c8518299b0df4a2a66d0e236
Mahdy Darroudy <146613339+darroudi82@users.noreply.github.com>
2023-11-08 12:49:50 +0330

    added for class13

---
+++ PHP-exercise13/main.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>main page</title>
+</head>
+<body>
+ <h1>main page</h1>
+ <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora expedita illum recusandae ad praesentium ea nesciunt repellat error dicta, autem, minima pariatur ullam hic eveniet quas consectetur minus soluta perspiciatis labore. Praesentium.</p>
+</body>
+</html>
